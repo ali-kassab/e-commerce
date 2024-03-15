48285976bc37732541325fae44f5ad3fd29c2097
alikassab <alikassab155@gmail.com>
2024-03-15 15:45:09 +0200

    edit database

--- Database/Connection.js
+++ Database/Connection.js
@@ -1,7 +1,7 @@
 
 import mongoose from 'mongoose';
 export const DBconnection = () => {
-    mongoose.connect('mongodb+srv://alikassab:3Wu!*ghTy25$tr@cluster0.2wxsgip.mongodb.net/Ecommerce').then(() => {
+    mongoose.connect(process.env.HOST).then(() => {
         console.log('database connected');
     }).catch((error) => {
         console.log(error);
